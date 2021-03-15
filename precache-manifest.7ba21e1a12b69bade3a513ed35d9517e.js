ccd4759aff9c75033e5c91a8dfddabf2111da6d8
Gazi Jarin <gazijarin@gmail.com>
2021-03-14 21:31:03 -0400

    Updates

---
+++ precache-manifest.7ba21e1a12b69bade3a513ed35d9517e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b2a6321ec94a3c40aed0f50b6644ac",
+    "url": "/gazi/index.html"
+  },
+  {
+    "revision": "65fb08d3df4d0980d109",
+    "url": "/gazi/static/css/2.734ab464.chunk.css"
+  },
+  {
+    "revision": "a23f99fc6615335ebed2",
+    "url": "/gazi/static/css/main.135ecf53.chunk.css"
+  },
+  {
+    "revision": "65fb08d3df4d0980d109",
+    "url": "/gazi/static/js/2.c2bb8334.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/gazi/static/js/2.c2bb8334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23f99fc6615335ebed2",
+    "url": "/gazi/static/js/main.bfc63e1a.chunk.js"
+  },
+  {
+    "revision": "ec847240dc1437678902",
+    "url": "/gazi/static/js/runtime-main.e00f1405.js"
+  },
+  {
+    "revision": "67ad497bef862bc00e976040d2bbc8e3",
+    "url": "/gazi/static/media/me.67ad497b.jpg"
+  }
+]);
