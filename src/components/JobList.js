a26a99f131abf16f6d60cefd04c14d274995d16f
Gazi Jarin <gazijarin@Gazis-Mac-Studio.local>
2023-12-24 20:18:46 -0500

    update job list

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -86,12 +86,12 @@ const JobList = () => {
       jobTitle: "Software Development Engineer @",
       duration: "JUL 2022 - PRESENT",
       desc: [
-        "Led development of end-to-end region build automation across Route 53 (AWS's DNS web service).  This enabled the launch of customer-facing global services in new regions within a day, a significant reduction} from the previous time-frame of a month.",
+        "Led development of end-to-end region build automation across Route 53 (AWS's DNS web service).  This enabled the launch of customer-facing global services in new regions within a day, a significant reduction from the previous time-frame of a month.",
         "Re-built Route 53's core domain management and DNS systems to provide a better user experience to millions of customers."
       ]
     },
     Wattpad: {
-      jobTitle: "Software Engineering Intern @",
+      jobTitle: "Associate Engineer @",
       duration: "MAY 2020 - APR 2021",
       desc: [
         "Developed a responsive React web page (the new Story Details) from scratch, both on client and server side, for an app with massive scale (2 billion daily requests).",
@@ -103,18 +103,18 @@ const JobList = () => {
       jobTitle: "Research Engineer @",
       duration: "MAY 2021 - SEPT 2021",
       desc: [
-        "Developing and researching an NLP-based framework using state-of-the-art tools like Spacy and Stanza to facilitate the derivation of requirements from health data by leveraging syntactic dependencies, entity-recognition and rule-based match-making.",
+        "Developed and researched an NLP-based framework using state-of-the-art tools like Spacy and Stanza to facilitate the derivation of requirements from health data by leveraging syntactic dependencies, entity-recognition and rule-based match-making.",
         " Application selected for DCS Research Award ($4,000) as part of the ”Visualizing Privacy Analysis Results” project led by Professor Marsha Chechik."
       ]
     },
-    // Centivizer: {
-    //   jobTitle: "Research Developer @",
-    //   duration: "SEPT 2019 - APR 2020",
-    //   desc: [
-    //     "Researched and developed interactive and neural-activation technologies to stimulate physical and cognitive functions in order to slow the progression of neurodegenerative disorders.",
-    //     "Leveraged WebRTC to develop and maintain a Node.js online video-streaming platform in real-time competitive-mode games to research the effects of active stimulation for those suffering from dementia."
-    //   ]
-    // },
+    Centivizer: {
+      jobTitle: "Software Developer @",
+      duration: "SEPT 2019 - APR 2020",
+      desc: [
+        "Developed interactive and neural-activation technologies to stimulate physical and cognitive functions in order to slow the progression of neurodegenerative disorders.",
+        "Leveraged WebRTC to develop and maintain a Node.js online video-streaming platform in real-time competitive-mode games to research the effects of active stimulation for those suffering from dementia."
+      ]
+    },
     // TDSB: {
     //   jobTitle: "Software Engineer @",
     //   duration: "SEPT 2019 - DEC 2020",
@@ -123,10 +123,10 @@ const JobList = () => {
     //   ]
     // },
     "Orange Gate": {
-      jobTitle: "Software Engineering Intern @",
+      jobTitle: "Software Developer Intern @",
       duration: "MAY 2019 - AUG 2019",
       desc: [
-        "Developed a Node.js smart home system through Facebook’s Messenger integrated with Bocco sen- sors and other smart devices (Nest camera, TPLink smart plugs) to derive conclusions about the current state of the home",
+        "Developed a Node.js smart home system through Facebook’s Messenger integrated with Bocco sensors and other smart devices (Nest camera, TPLink smart plugs) to derive conclusions about the current state of the home",
         "Identified continuous improvements in data quality, design reports and coding activities, presenting results and findings to internal business stakeholders.",
         "Relevant technologies/tools used: DialogFlow, Vision, AutoML, Messenger Bot API, MongoDB."
       ]
